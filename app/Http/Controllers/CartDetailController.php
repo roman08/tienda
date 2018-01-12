74ec129d3ae4edbdbfc4c0d3711a9a4c48387d86
roman08 <rmcentinela@gmail.com>
2018-01-12 05:44:29 -0600

    se cargan cambio validacion

--- app/Http/Controllers/CartDetailController.php
+++ app/Http/Controllers/CartDetailController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\CartDetail;
 class CartDetailController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
